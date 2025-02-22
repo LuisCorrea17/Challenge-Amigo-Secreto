8e8e36344beac30d0f404ac38c755fb64bce6a2e
CORREA Luis M. TENARIS <LMCORREA@tenaris.com>
2025-02-22 16:59:41 -0600

    Add function agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo() {
+    let textInput = document.querySelector('.input-name');
+    if (textInput.value == '') {
+        alert("Por favor, inserte un nombre");
+    } else {
+        amigos.push(textInput);
+        textInput.value = '';
+    }
+}
